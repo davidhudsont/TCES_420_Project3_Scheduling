addc1d36a04e693af51c63d50832feb3529fdbbd
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-10-31 08:06:51 -0700

    Add files via upload

--- scheduler.c
+++ scheduler.c
@@ -1,95 +1,60 @@
-
-
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-#define SIZE 4
+#define qSIZE 4
+#define jSIZE 4
 
 typedef struct Job {
-	int job_id; // 4
-
-    	int NR_PHASES;  // 4
-
-	int current_phase; // 4
-
-	// Phase types: 1 = CPU phase; 2 = IO phase
-
-	//int phasetype_and_duration[NR_PHASES][NR_PHASES];
-
-	int is_completed; // 4
+	int job_id; 
+	int duration;
+	int current_phase;
+	int is_completed; 
 	
-}job; // 4+4+4+4=16
+}job; 
 
 typedef struct Queue {
-	//int* runqueue;
+	
 	job* jobqueue; 
 	int head,tail;
-	//*************job* head = jobqueue, tail = head;
-	// move tail when ever you add someting
 }queue;
-/*
-int enqueue(queue run,int x) {
-	for (int i=0; i<SIZE; i++) {
-		if (run.runqueue[i]==0) {
-			run.runqueue[i] = x;
-			return x;
-		}
-		else {
-			
-		}
-	}
-	return 0;
-}
-int dequeue(queue run) {
-	int first = run.runqueue[0];
-	for (int i=0; i<SIZE; i++) {
-		if (i>=SIZE-1) {
-			run.runqueue[i] = 0;
-			break;
-		}
-		run.runqueue[i] = run.runqueue[i+1];
-	}
-	return first;
-}
-*/
+
 
 void enqueue(queue *a,job b) {
-	for (int i=0; i<SIZE; i++) {
-		if (a->tail==4 && a->head!=a->tail) {// a-> is the same as (*a).tail = 
+	for (int i=0; i<qSIZE; i++) {
+		if (a->tail==4 && a->head!=a->tail) {
 			printf("Queue is full!\n");
 			return;
 		}
-		if (a->head==a->tail) {// head value == tail value only true if address was already the same value
-			a->head =0; 
+		if (a->head==a->tail) {
+			a->head =0;
 			a->tail =0;
 			a->jobqueue[i]=b;
 			a->tail++;
-			printf("tail: %d, b.job_id: %d, a->jobqueue[i]: %d\n",a->tail,b.job_id,a->jobqueue[i].job_id);
 			return;
 		}
 		if (i==a->tail) {
 			a->jobqueue[i]=b;
 			a->tail= a->tail+1;
-			printf("tail: %d, b.job_id: %d, a->jobqueue[i]: %d\n",a->tail,b.job_id,a->jobqueue[i].job_id);
 			return;
 		}
 	}
 }
 
-job dequeue(queue *a) {
-	for (int i=0; i<SIZE; i++) {
+job* dequeue(queue *a) {
+	for (int i=0; i<qSIZE; i++) {
 		if (a->head == a->tail && a->tail!=0) {
 			a->head = 0;
 		}
 		if (i==a->head) {
-			job ret = a->jobqueue[i];
+			job *ret = &a->jobqueue[i];
 			a->head++;
 			return ret;
 		}
 	}
 }
 
-int size(queue *a) {
+int queue_size(queue *a) {
 	int quantity=0;
 	for (int i=a->head; i<a->tail; i++) {
 		quantity++;
@@ -97,47 +62,48 @@ int size(queue *a) {
 	return quantity;
 }
 
+void init_job(job* j,int id,int time,int crt_phase, int is_comp) {
+	j->job_id = id;
+	j->duration = time;
+	j->current_phase = crt_phase;
+	j->is_completed = is_comp;
+	return;
+}
+queue run;
+queue io;
+queue done;
+queue *run_ptr = &run;
+queue *io_ptr = &io;
+queue *done_ptr = &done;
+
+void delay(int time){
+	int t = clock();
+	while ((float)((clock()-t)/CLOCKS_PER_SEC) <time) {}
+}
+
 int main() {
-	queue run;
-	job job1;
-	run.jobqueue = (job*)malloc(SIZE*sizeof(job));
-	printf("%d\n",sizeof(job));
-	job1.job_id = 42;
-	job job2;
-	job2.job_id = 23;
-	job job3;
-	job3.job_id = 33;
-	job job4;
-	job4.job_id = 1;
-	job job5;
-	job5.job_id = 3;
+	run.jobqueue = (job*)calloc(qSIZE,sizeof(job));
+	io.jobqueue = (job*)calloc(qSIZE,sizeof(job));
+	//done.jobqueue = (job*)malloc(qSIZE*sizeof(job));
+	srand(time(NULL));
+		
+	for (int i=0; i<jSIZE; i++) {
+		job* j = (job*)malloc(sizeof(job));
+		int dur = rand()%5;
+		init_job(j,i+1,dur,0,0);
+		enqueue(run_ptr,*j);
+	}
+	for (int i=0; i<queue_size(run_ptr); i++) {
+		printf("job_id: %d job_dur: %d\n", run.jobqueue[i].job_id,run.jobqueue[i].duration);
+	}
+	for (int i=0; i<jSIZE; i++) {
+		job* j = dequeue(run_ptr);
+		delay(j->duration);
+		printf("Job Finished\n");
+		enqueue(io_ptr,*j);
+		printf("job_id: %d job_dur: %d\n", io.jobqueue[i].job_id,io.jobqueue[i].duration);
+	}
 	
-	run.head = 0;
-	run.tail = 0;
-	queue *p;
-	p = &run;
-	enqueue(p,job1);
-	enqueue(p,job2);
-	enqueue(p,job3);
-	enqueue(p,job4);
-	enqueue(p,job5);
-	for (int i=0; i<run.tail; i++) {
-		printf("job_id: %d\n", run.jobqueue[i].job_id);
-	}
-	printf("qsize: %d\n",size(p));
-	printf("tail: %d head %d\n",run.tail,run.head);
-	dequeue(p);
-	dequeue(p);
-	dequeue(p);
-	dequeue(p);
-	printf("tail: %d head %d\n",run.tail,run.head);
-	printf("qsize: %d\n",size(p));
-	enqueue(p,job5);
-	for (int i=0; i<run.tail; i++) {
-		printf("job_id: %d\n", run.jobqueue[i].job_id);
-	}
-	printf("qsize: %d\n",size(p));
-	printf("tail: %d head %d\n",run.tail,run.head);
 	return 0;
 	
 	
