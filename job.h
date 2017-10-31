528f8de1cfe9d7876b09bfc495fbe9428964c589
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-10-31 12:00:13 -0700

    Update job.h

--- job.h
+++ job.h
@@ -4,10 +4,9 @@
 
 typedef struct Job {
 	int job_id; 
-	int duration;
-	int current_phase;
-	int is_completed; 
-	
+	int cpu_duration;
+	int io_duration;
+	int current_phase; 
 }job; 
 
-void init_job(job* j,int id,int time,int crt_phase, int is_comp);
+void init_job(job* j,int id,int cpu,int io, int phase);
