a98ebdb449e3ad0631fe86f54f84716d6bea9459
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-10-31 18:10:12 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -5,22 +5,19 @@
 #ifndef LIST_H_   /* Include guard */
 #define LIST_H_
 
-typedef struct node_tag {
-   job* data;
-   struct node_tag *next;
-} Node;
-
 typedef struct Queue {
-	Node* head;
-	Node* tail;
-}queue;
-
-
-void enqueue(queue *q,job j);
-
-job* dequeue(queue *q);
-
-void queue_init(queue *q,int size);
-
+ 	int qsize;	
+ 	int head,tail;
+ 	job* jobqueue; 
+ }queue;
+ 
+ 
+ void enqueue(queue *q,job j);
+ 
+ job* dequeue(queue *q);
+ 
+ int queue_size(queue *q);
+ 
+ void queue_init(queue *q,int size);
 
 #endif 
