8e30dcf4e9568af63baba3fa775a10f038c7fcd9
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-02 11:12:47 -0700

    Add files via upload

--- job.c
+++ job.c
@@ -1,12 +1,31 @@
 
+#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <job.h>
 
 
-void init_job(job* j,int id,int cpu,int io, int phase) {
+
+void init_job(job* j,int id) {
+	srand(time(NULL));
+	j->is_completed = 0;
 	j->job_id = id;
-	j->cpu_duration = cpu;
-	j->io_duration = io;
-	j->current_phase = phase;
+	j->tasks = rand()%4+2;
+	j->phases = (int **)malloc(j->tasks*sizeof(int*));
+	for (int i=0; i<2; i++) {
+		j->phases[i] = (int*)malloc(2*sizeof(int));
+	}
+	for (int col=0; col<j->tasks; col++) {
+			int phase_type = rand()%2;
+			if (phase_type ==1) {
+				j->phases[1][col] = 1;
+				j->phases[0][col] = rand()%4+7;
+			}
+			else {
+				j->phases[1][col] = 0;
+				j->phases[0][col] = rand()%4+1;
+			}
+		}
+	j->current_phase = j->phases[1][0];
 	return;
 }
