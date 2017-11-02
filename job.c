493dd4d5242a8d17ecba899d292e314c01a9de57
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-02 11:18:07 -0700

    Update job.c

--- job.c
+++ job.c
@@ -11,21 +11,21 @@ void init_job(job* j,int id) {
 	j->is_completed = 0;
 	j->job_id = id;
 	j->tasks = rand()%4+2;
+	j->current_phase = 0;
 	j->phases = (int **)malloc(j->tasks*sizeof(int*));
 	for (int i=0; i<2; i++) {
 		j->phases[i] = (int*)malloc(2*sizeof(int));
 	}
 	for (int col=0; col<j->tasks; col++) {
-			int phase_type = rand()%2;
-			if (phase_type ==1) {
-				j->phases[1][col] = 1;
-				j->phases[0][col] = rand()%4+7;
-			}
-			else {
-				j->phases[1][col] = 0;
-				j->phases[0][col] = rand()%4+1;
-			}
+		int phase_type = rand()%2;
+		if (phase_type ==1) {
+			j->phases[1][col] = 1;
+			j->phases[0][col] = rand()%4+7;
 		}
-	j->current_phase = j->phases[1][0];
+		else {
+			j->phases[1][col] = 0;
+			j->phases[0][col] = rand()%4+1;
+		}
+	}
 	return;
 }
