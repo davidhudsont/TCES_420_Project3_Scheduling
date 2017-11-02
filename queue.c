fe8bcad99f907b0841bea3e229c4edf171e3cbc5
thien1775 <thien1775@yahoo.com>
2017-11-01 18:27:23 -0700

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,70 +1,35 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <queue.h>
 
 
+int isEmpty(queue *q){
+	return (q->qsize ==0);	
+}	    
+
+int isFull(queue *q){
+	return (q->qsize ==q->qcapacity);	
+}
+
+void queue_init(queue *q,int capacity) {
+	q->jobqueue = (job*)calloc(capacity,sizeof(job));
+	q->qcapacity = capacity;
+	q->head = 0;
+	q->tail = capacity-1;
+	q->qsize = 0;
+}
+
 void enqueue(queue *q,job j) {
-	struct Node *newNode;
-    newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = j;
-    newNode->next = NULL;
-    if(queue->head == NULL);
-        queue->head = newNode;
-        queue->tail = newNode;
-    else{
-        queue->tail->next = newNode;
-        queue->tail = newNode;
-    }
-    printf("enqueue is Success!!!\n");
-    /*for (int i=0; i<q->qsize; i++) {
-		if (q->tail==q->qsize && q->head!=q->tail) {
-			printf("Queue is full!\n");
-			return;
-		}
-		if (q->head==q->tail) {
-			q->head =0;
-			q->tail =0;
-			q->jobqueue[i]=j;
-			q->tail++;
-			return;
-		}
-		if (i==q->tail) {
-			q->jobqueue[i]=j;
-			q->tail++;
-			return;
-		}
-	}*/
+	if (isFull(q)) { return; } 
+	q->tail = (q->tail + 1)%q->qcapacity;
+	q->jobqueue[q->tail] = j;
+	q->qsize++;
 }
 
 job* dequeue(queue *q) {
-	if(queue->head == NULL){
-        printf("\nQueue is Empty!!!\n");
-        return NULL;
-    }
-    struct Node *temp = queue->head;
-    printf("\n element: %d\n",  queue->head->data);
-    queue->head = queue->head->next;
-    if( queue->head == NULL){
-        q->tail = NULL;
-    }
-    return temp->data;
-    
-    
-    /*for (int i=0; i<q->qsize; i++) {
-		if (q->head == q->tail && q->tail!=0) {
-			q->head = 0;
-		}
-		if (i==q->head) {
-			job *ret = &q->jobqueue[i];
-			q->head++;
-			return ret;
-		}
-	}*/
-}
-
-
-void queue_init(queue *q,int size) {
-	q->head = NULL;
-	q->tail = NULL;
+	if (isEmpty(q)) { return NULL;}
+	job *ret = &q->jobqueue[q->head];
+	q->head = (q->head+1)%q->qcapacity;
+	q->qsize--;
+	return ret;
 }
