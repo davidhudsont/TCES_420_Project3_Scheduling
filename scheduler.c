1b448c54f8664d3d11d0df85c8eb079cefb71dd9
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-04 12:10:25 -0700

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <queue.h>
+#include <assert.h>
+//#include <pthread.h>
+//#include <unistd.h>
+#define qSIZE 100
 
-#define qSIZE 4
-#define jSIZE 4
+// unsigned int sleep(unsigned int seconds);
 
-int job_counter;
 queue run;
 queue io;
 queue done;
@@ -14,12 +16,24 @@ queue *run_ptr = &run;
 queue *io_ptr = &io;
 queue *done_ptr = &done;
 
-void delay(int time){
-	int t = clock();
-	while ((float)((clock()-t)/CLOCKS_PER_SEC) <time) {}
-}
-
 int main() {
-	
+	void * arg;
+	// pthread_t cpu[8];
+	// pthread_t io[4];
+	// pthread_t job_submission[4];
+	/*
+	for (int i=0; i<8; i++) {
+		int rc = pthread_create(&cpu,NULL,cpu_thread,arg);
+		asssert(rc == 0);
+	}
+	for (int i=0; i<4; i++) {
+		int rc = pthread_create(&cpu,NULL,io_thread,arg);
+		asssert(rc == 0);
+	}
+	for (int i=0; i<4; i++) {
+		int rc = pthread_create(&cpu,NULL,job_submission_thread,arg);
+		asssert(rc == 0);
+	}
+	*/
 	return 0;
 }
