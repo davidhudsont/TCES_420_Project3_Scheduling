5e0d6ba2548f6d0d423350883b3ca3cb62932a96
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-05 11:36:16 -0800

    Add files via upload

--- job.h
+++ job.h
@@ -4,9 +4,10 @@
 
 typedef struct Job {
 	int job_id; 
-	int cpu_duration;
-	int io_duration;
+	int** phases;
+	int tasks;
 	int current_phase; 
+	int is_completed;
 }job; 
 
 void init_job(job* j,int id);
