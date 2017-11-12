e6c7e42656276aa2666d76e0b975edbd7ec71779
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-12 11:45:20 -0800

    Update queue.c

--- queue.c
+++ queue.c
@@ -3,24 +3,16 @@
 #include <stdio.h>
 #include <queue.h>
 
+int isEmpty(queue *q){ return (q->qsize ==0);	}	    
 
-int isEmpty(queue *q){
-	return (q->qsize ==0);	
-}	    
+void queue_init(queue *q) { q->qsize = 0; }
 
+void queue_delete(queue *q) { free(q); }
 
-void queue_init(queue *q) {
-	//q->head = (node*)malloc(sizeof(node)); 
-	q->qsize = 0;
-}
-
-void queue_delete(queue *q) {
-	free(q);
-}
 void enqueue(queue *q,job *a) {
-    node *temp = (node*)malloc(sizeof(node));
-    temp->j = a;
-    temp->next = NULL;
+    	node *temp = (node*)malloc(sizeof(node));
+    	temp->j = a;
+    	temp->next = NULL;
 	if (q->qsize==0){
 		q->head = temp;
 		q->tail = temp;
@@ -29,19 +21,19 @@ void enqueue(queue *q,job *a) {
 		q->tail->next = temp;
 		q->tail = temp;
 	}
-    q->qsize++;
+    	q->qsize++;
 }
 
 job* dequeue(queue *q) {
 	if (isEmpty(q)) {
 		return NULL;
 	}
-    job* js = q->head->j;
-    node* temp = q->head;
-    temp->j = NULL;
-    q->head = q->head->next;
-    q->qsize--;
-    free(temp);
-    return js;
+    	job* js = q->head->j;
+    	node* temp = q->head;
+    	temp->j = NULL;
+    	q->head = q->head->next;
+    	q->qsize--;
+    	free(temp);
+    	return js;
 }
 
